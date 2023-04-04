b9a16844ac43e754278476da0fa24007ac82aba9
Blackout03 <gardner.hayden03@gmail.com>
2023-04-04 14:48:05 +1000

    v1.1.0 - For 1.18.2

--- src/main/java/com/blackout/nofallingblocks/NoFallingBlocks.java
+++ src/main/java/com/blackout/nofallingblocks/NoFallingBlocks.java
@@ -1,7 +1,6 @@
 package com.blackout.nofallingblocks;
 
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.GameRules;
+import net.minecraft.world.level.GameRules;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
@@ -13,29 +12,27 @@ import org.apache.maven.artifact.versioning.ArtifactVersion;
 
 import java.util.Optional;
 
-@Mod("nofallingblocks")
+@Mod(NoFallingBlocks.MODID)
 public class NoFallingBlocks {
 	public static final String MODID = "nofallingblocks";
 	public static final String MODNAME = "No Falling Blocks";
 	public static ArtifactVersion VERSION = null;
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-	public static final GameRules.RuleKey<GameRules.BooleanValue> PREVENT_BLOCK_FALLING_RULE = GameRules.register("preventBlockFalling", GameRules.Category.UPDATES, GameRules.BooleanValue.create(true));
+	public static final GameRules.Key<GameRules.BooleanValue> PREVENT_BLOCK_FALLING_RULE = GameRules.register("preventBlockFalling", GameRules.Category.UPDATES, GameRules.BooleanValue.create(true));
 
 	public NoFallingBlocks() {
 		Optional<? extends ModContainer> opt = ModList.get().getModContainerById(MODID);
 		if (opt.isPresent()) {
 			IModInfo modInfo = opt.get().getModInfo();
 			VERSION = modInfo.getVersion();
-		} else LOGGER.warn("Cannot get version from mod info");
+		} else {
+			LOGGER.warn("Cannot get version from mod info");
+		}
 
 		LOGGER.debug(MODNAME + " Version is: " + VERSION);
 		LOGGER.debug("Mod ID for " + MODNAME + " is: " + MODID);
 
 		MinecraftForge.EVENT_BUS.register(this);
-	}
-
-	public static ResourceLocation location(String name) {
-		return new ResourceLocation(MODID, name);
 	}
 }
---
+++ src/main/java/com/blackout/nofallingblocks/mixins/FallingBlockMixin.java
@@ -0,0 +1,36 @@
+package com.blackout.nofallingblocks.mixins;
+
+import com.blackout.nofallingblocks.NoFallingBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(FallingBlock.class)
+public abstract class FallingBlockMixin extends Block {
+	public FallingBlockMixin(Properties properties) {
+		super(properties);
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+	private void cancelFalling(BlockState blockState, ServerLevel level, BlockPos blockPos, Random random, CallbackInfo callbackInfo) {
+		if (level.getGameRules().getRule(NoFallingBlocks.PREVENT_BLOCK_FALLING_RULE).get()) {
+			callbackInfo.cancel();
+		}
+	}
+
+	@Inject(method = "animateTick", at = @At("HEAD"), cancellable = true)
+	private void cancelFallingParticle(BlockState blockState, Level level, BlockPos blockPos, Random random, CallbackInfo callbackInfo) {
+		if (level.getGameRules().getRule(NoFallingBlocks.PREVENT_BLOCK_FALLING_RULE).get()) {
+			callbackInfo.cancel();
+		}
+	}
+}
